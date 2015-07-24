9037435a135e92f65f84e775df9725582c3d5c04
Khashie <khashie@hotmail.com>
2015-07-24 11:29:16 -0700

    done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,42 @@
 $(document).ready(function(){
 
+
+$(document).keydown(function(key){
+	switch(parseInt(key.which,10)) {
+	case 88:
+	$('.ryu-ready').hide();
+	$('.ryu-still').hide();
+	$('.ryu-throwing').hide();
+	$('.ryu-cool').show();
+	break;
+
+}
+
+})
+
+$(document).keyup(function(key){
+	switch(parseInt(key.which,10)) {
+	case 88:
+	$('.ryu-cool').hide();
+	$('.ryu-throwing').hide();
+	$('.ryu-still').hide();
+	$('.ryu-ready').show();
+	break;
+
+}
+
+})
+
+
+function playHadouken () {
+
+	$('#hadouken-sound')[0].volume=0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
+
+
+
 $('.ryu').mouseenter(function() {
 
 	$('.ryu-still').hide();
@@ -16,9 +53,13 @@ $('.ryu').mouseenter(function() {
 })
 
 .mousedown(function() {
+	playHadouken();
 $('.ryu-ready').hide();
+$('.ryu-still').hide();
+$('.ryu-cool').hide();
 $('.ryu-throwing').show();
 $('.hadouken')
+	.finish()
 	.show()
 	.animate({'left':'1020px'},500,function(){
 		$(this).hide();
